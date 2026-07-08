96c26601874691823d89b6cce42d740780586194
Lorenzo Waldner <lorenzo.waldner@sidera.it>
2026-07-08 22:48:29 +0200

    (#_) Gestisce il ciclo di vita delle istanze Playwright e Browser

--- src/main/java/com/sidera/gate/GateAutomationService.java
+++ src/main/java/com/sidera/gate/GateAutomationService.java
@@ -6,6 +6,7 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,57 +15,64 @@ import org.springframework.stereotype.Service;
 public class GateAutomationService {
 
 	private static final Logger log = LoggerFactory.getLogger(GateAutomationService.class);
+	private final Playwright playwright;
+	private final Browser browser;
+
+	public GateAutomationService() {
+		this.playwright = Playwright.create();
+		log.info("Istanza Playwright creata.");
+
+		this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+			.setHeadless(true));
+		log.info("Browser Chromium avviato in modalita headless.");
+	}
 
 	public synchronized void openCommand(String targetTitle) {
 		log.info("Avvio automazione Playwright per target '{}'.", targetTitle);
 
-		try (Playwright playwright = Playwright.create()) {
-			log.info("Istanza Playwright creata.");
+		try (BrowserContext context = browser.newContext()) {
+			log.info("Browser context creato.");
 
-			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-				.setHeadless(true));
-			log.info("Browser Chromium avviato in modalita headless.");
+			Page page = context.newPage();
+			log.info("Nuova pagina creata.");
 
-			try (browser) {
-				BrowserContext context = browser.newContext();
-				log.info("Browser context creato.");
+			log.info("Navigazione verso la pagina di login.");
+			page.navigate("http://192.168.1.2/login.php");
 
-				try (context) {
-					Page page = context.newPage();
-					log.info("Nuova pagina creata.");
+			log.info("Click sul campo utente.");
+			page.locator("input[name=\"User\"]").click();
 
-					log.info("Navigazione verso la pagina di login.");
-					page.navigate("http://192.168.1.2/login.php");
+			log.info("Inserimento username.");
+			page.locator("input[name=\"User\"]").fill("admin");
 
-					log.info("Click sul campo utente.");
-					page.locator("input[name=\"User\"]").click();
+			log.info("Passaggio al campo password.");
+			page.locator("input[name=\"User\"]").press("Tab");
 
-					log.info("Inserimento username.");
-					page.locator("input[name=\"User\"]").fill("admin");
+			log.info("Inserimento password.");
+			page.locator("input[name=\"Password\"]").fill("password");
 
-					log.info("Passaggio al campo password.");
-					page.locator("input[name=\"User\"]").press("Tab");
+			log.info("Click sul pulsante Login.");
+			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
 
-					log.info("Inserimento password.");
-					page.locator("input[name=\"Password\"]").fill("password");
+			log.info("Selezione della voce 'Casa Andreis'.");
+			page.getByText("Casa Andreis").click();
 
-					log.info("Click sul pulsante Login.");
-					page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+			log.info("Click sul comando finale '{}'.", targetTitle);
+			page.getByTitle(targetTitle).click();
 
-					log.info("Selezione della voce 'Casa Andreis'.");
-					page.getByText("Casa Andreis").click();
-
-					log.info("Click sul comando finale '{}'.", targetTitle);
-					page.getByTitle(targetTitle).click();
-
-					log.info("Automazione completata con successo per target '{}'.", targetTitle);
-				}
-			}
+			log.info("Automazione completata con successo per target '{}'.", targetTitle);
 		} catch (Exception exception) {
 			log.error("Automazione Playwright fallita per target '{}': {}", targetTitle, exception.getMessage(), exception);
 			throw exception;
 		} finally {
 			log.info("Chiusura automazione Playwright per target '{}'.", targetTitle);
 		}
+	}
+
+	@PreDestroy
+	public void shutdown() {
+		log.info("Chiusura browser Playwright.");
+		browser.close();
+		playwright.close();
 	}
 }
